2c5ee60fc8087e23c0b85985f161c666cc0318f5
Jaanu2001-web <jananirk01@gmail.com>
2024-08-09 10:21:46 +0530

    files added

---
+++ android/app/src/main/kotlin/com/example/gorouter2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gorouter2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gorouter2/models/user.dart';
+import 'package:modular_ui/modular_ui.dart';
+
+class ProfilePage extends StatelessWidget {
+  final User user;
+
+  const ProfilePage({
+    super.key,
+    required this.user,
+  });
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Profile",
+        ),
+      ),
+      body: Center(
+        child: MUIProfileCard(
+          name: user.name,
+          image: Image.network(
+            user.imageURL,
+          ),
+          designation: user.role,
+        ),
+      ),
+    );
+  }
+}
